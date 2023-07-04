7f35a8b05557a27e0891e833c5643a185cb774f2
Phunmey <funmilola@aims.edu.gh>
2023-07-03 19:35:05 -0500

    VR and Alpha computation

---
+++ src/filtration/alpha_pca_bettig_filtration.py
@@ -0,0 +1,164 @@
+import random
+from datetime import datetime
+from time import time
+import numpy as np
+import pandas as pd
+from igraph import *
+import gudhi as gd
+from sklearn.decomposition import PCA
+
+random.seed(42)
+
+
+def reading_csv(dataset):
+    df_edges = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_A.txt", header=None)  # import edge data
+    df_edges.columns = ['from', 'to']
+    print("Graph edges are loaded")
+    csv = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_graph_indicator.txt", header=None)
+    print("Graph indicators are loaded")
+    csv.columns = ["ID"]
+    graph_indicators = (csv["ID"].values.astype(int))
+    read_csv = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_graph_labels.txt", header=None)
+    read_csv.columns = ["ID"]
+    graph_labels = (read_csv["ID"].values.astype(int))
+    print("Graph labels are loaded")
+    unique_graph_indicator = np.arange(min(graph_indicators),
+                                       max(graph_indicators) + 1)  # list unique graph ids in a dataset
+
+    return unique_graph_indicator, graph_labels, df_edges, graph_indicators
+
+
+def alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, dataset,
+               graph_labels):  # this is for the train data
+
+    betti_list = []
+    density = []
+    diameter = []
+    clustering_coeff = []
+    spectral_gap = []
+    assortativity_ = []
+    cliques = []
+    motifs = []
+    components = []
+
+    for graph_id in unique_graph_indicator:
+        try:
+            id_location = [index + 1 for index, element in enumerate(graph_indicators) if
+                           element == graph_id]  # list the index of the graph_id locations
+            graph_label = [ele for ind, ele in enumerate(graph_labels, start=1) if
+                           ind == graph_id]  # obtain graph label corresponding to the graph_id
+            graph_edges = df_edges[df_edges['from'].isin(id_location)]
+            create_graph = Graph.TupleList(graph_edges.itertuples(index=False), directed=False, weights=True)
+            # plot(create_traingraph)
+
+            if not create_graph.is_connected():
+                graph_decompose = create_graph.decompose()
+                mds_list = []
+                for subg in graph_decompose:
+                    create_subg = np.asarray(Graph.shortest_paths_dijkstra(subg))
+                    norm_subg = create_subg / np.nanmax(create_subg)
+                    mds = PCA(n_components=2).fit_transform(norm_subg)
+                    mds_list.append(mds)
+                matrix_mds = (np.vstack(mds_list))
+            else:
+                create_dmatrix = np.asarray(Graph.shortest_paths_dijkstra(create_graph))
+                norm_dmatrix = create_dmatrix / np.nanmax(create_dmatrix)
+                matrix_mds = PCA(n_components=2).fit_transform(norm_dmatrix)
+
+            start = time()
+            ac = gd.AlphaComplex(points=matrix_mds).create_simplex_tree()
+            dgm = ac.persistence()  # obtain persistence values
+            filtr_time = time() - start
+
+            #    select dimensions 0 and 1
+            dgm_0 = ac.persistence_intervals_in_dimension(0)
+            dgm_1 = ac.persistence_intervals_in_dimension(1)
+
+            # save the persistence diagrams
+            filename = "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/persistentDiagram/" + dataset + str(
+                step_size) + "pbettig.txt"
+            with open(filename, "a") as f:
+                f.write(f"{graph_id}: {dgm_0, dgm_1}\n")
+                f.flush()  # ensures data is written to file
+                f.close()
+
+            #    obtain betti numbers for the unique dimensions
+            betti_0 = []
+            betti_1 = []
+
+            for eps in np.linspace(0, 1, step_size):
+                b_0 = 0
+                for k in dgm_0:
+                    if k[0] <= eps and k[1] > eps:
+                        b_0 = b_0 + 1
+                betti_0.append(b_0)
+
+                b_1 = 0
+                for l in dgm_1:
+                    if l[0] <= eps and l[1] > eps:
+                        b_1 = b_1 + 1
+                betti_1.append(b_1)
+
+            betti_list.append(
+                [dataset] + [graph_id] + graph_label + [filtr_time] + betti_0 + betti_1)  # concatenate betti numbers
+
+            Density = create_graph.density()  # obtain density
+            Diameter = create_graph.diameter()  # obtain diameter
+            cluster_coeff = create_graph.transitivity_avglocal_undirected()  # obtain transitivity
+            laplacian = create_graph.laplacian()  # obtain laplacian matrix
+            laplace_eigenvalue = np.linalg.eig(laplacian)
+            sort_eigenvalue = sorted(np.real(laplace_eigenvalue[0]), reverse=True)
+            spectral = sort_eigenvalue[0] - sort_eigenvalue[1]  # obtain spectral gap
+            assortativity = create_graph.assortativity_degree()  # obtain assortativity
+            clique_count = create_graph.clique_number()  # obtain clique count
+            motifs_count = create_graph.motifs_randesu(size=3)  # obtain motif count
+            count_components = len(create_graph.clusters())  # obtain count components
+
+            density.append(Density)
+            diameter.append(Diameter)
+            clustering_coeff.append(cluster_coeff)
+            spectral_gap.append(spectral)
+            assortativity_.append(assortativity)
+            cliques.append(clique_count)
+            motifs.append(motifs_count)
+            components.append(count_components)
+
+        except:
+            pass
+
+    df1 = pd.DataFrame(betti_list)
+    df2 = pd.DataFrame(motifs)
+    df3 = pd.DataFrame(
+        list(zip(density, diameter, clustering_coeff, spectral_gap, assortativity_, cliques, components)))
+    feature_data = pd.concat([df1, df2, df3], axis=1, ignore_index=True)
+
+    #    giving column names
+    columnnames = {}  # create an empty dict
+    count = -4  # initialize count to -1
+    for i in feature_data.columns:
+        count += 1  # update count by 1
+        columnnames[i] = f"eps_{count}"  # index i in dictionary will be named thresh_count
+
+    # rename first and last column in the dictionary
+    columnnames.update(
+        {(list(columnnames))[0]: 'dataset', (list(columnnames))[1]: 'graphId', (list(columnnames))[2]: 'graphLabel',
+         (list(columnnames))[3]: 'filtrTime'})
+    feature_data.rename(columns=columnnames, inplace=True)  # give column names to dataframe
+
+    # write dataframe to file
+    feature_data.to_csv(
+        "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/result/filtration/" + dataset + str(
+            step_size) + "_alpha_pcabettig.csv", index=False)
+
+
+def main():
+    unique_graph_indicator, graph_labels, df_edges, graph_indicators = reading_csv(dataset)
+    alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, dataset, graph_labels)
+
+
+if __name__ == '__main__':
+    data_path = "/home/taiwo/projects/def-cakcora/taiwo/data"  # dataset path on computer
+    data_list = ('ENZYMES', 'BZR', 'MUTAG', 'PROTEINS', 'DHFR', 'NCI1', 'COX2', 'REDDIT-MULTI-5K', 'REDDIT-MULTI-12K')
+    for dataset in data_list:
+        for step_size in (10, 20, 50, 100):  # we will consider step size 100 for epsilon
+            main()
---
+++ src/filtration/rips_betti_filtration.py
@@ -0,0 +1,117 @@
+import random
+from datetime import datetime
+from time import time
+import numpy as np
+import pandas as pd
+from igraph import *
+from ripser import ripser
+
+random.seed(42)
+
+
+def reading_csv(dataset):
+    df_edges = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_A.txt", header=None)  # import edge data
+    df_edges.columns = ['from', 'to']
+    print("Graph edges are loaded")
+    csv = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_graph_indicator.txt", header=None)
+    print("Graph indicators are loaded")
+    csv.columns = ["ID"]
+    graph_indicators = (csv["ID"].values.astype(int))
+    read_csv = pd.read_csv(data_path + "/" + dataset + "/" + dataset + "_graph_labels.txt", header=None)
+    read_csv.columns = ["ID"]
+    graph_labels = (read_csv["ID"].values.astype(int))
+    print("Graph labels are loaded")
+    unique_graph_indicator = np.arange(min(graph_indicators),
+                                       max(graph_indicators) + 1)  # list unique graph ids in a dataset
+
+    return unique_graph_indicator, graph_labels, df_edges, graph_indicators
+
+
+def alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, dataset,
+               graph_labels, distance="spd"):  # this is for the train data
+
+    betti_list = []
+    for graph_id in unique_graph_indicator:
+        id_location = [index + 1 for index, element in enumerate(graph_indicators) if
+                       element == graph_id]  # list the index of the graph_id locations
+        graph_label = [ele for ind, ele in enumerate(graph_labels, start=1) if
+                       ind == graph_id]  # obtain graph label corresponding to the graph_id
+        graph_edges = df_edges[df_edges['from'].isin(id_location)]
+        create_traingraph = Graph.TupleList(graph_edges.itertuples(index=False), directed=False, weights=True)
+        if distance == "spd":
+            create_dmatrix = np.asarray(Graph.shortest_paths_dijkstra(create_traingraph))
+        elif distance == "resistance":
+            pass  # use resistance distance
+        else:
+            "this distance is not defined"
+        norm_dmatrix = create_dmatrix / np.nanmax(create_dmatrix[create_dmatrix != np.inf])
+
+        start = time()
+        train_rips = ripser(norm_dmatrix, thresh=1, maxdim=1, distance_matrix=True)[
+            'dgms']
+        filtr_time = time() - start
+
+        #    splitting the dimension into 0 and 1
+        train_dgm_0 = train_rips[0]
+        train_dgm_1 = train_rips[1]
+
+        # save the persistence diagrams
+        filename = "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/persistentDiagram/" + dataset + str(
+            step_size) + "ripsbetti.txt"
+        with open(filename, "a") as f:
+            f.write(f"{graph_id}: {train_dgm_0, train_dgm_1}\n")
+            f.flush()  # ensures data is written to file
+            f.close()
+
+        #    obtain betti numbers for the unique dimensions
+        train_betti_0 = []
+        train_betti_1 = []
+
+        for eps in np.linspace(0, 1, step_size):
+            b_0 = 0
+            for k in train_dgm_0:
+                if k[0] <= eps and k[1] > eps:
+                    b_0 = b_0 + 1
+            train_betti_0.append(b_0)
+
+            b_1 = 0
+            for l in train_dgm_1:
+                if l[0] <= eps and l[1] > eps:
+                    b_1 = b_1 + 1
+            train_betti_1.append(b_1)
+
+        betti_list.append([dataset] + [graph_id] + graph_label + [
+            filtr_time] + train_betti_0 + train_betti_1)  # concatenate betti numbers
+
+    feature_data = pd.DataFrame(betti_list)
+
+    #    giving column names
+    columnnames = {}  # create an empty dict
+    count = -4  # initialize count to -1
+    for i in feature_data.columns:
+        count += 1  # update count by 1
+        columnnames[i] = f"eps_{count}"  # index i in dictionary will be named thresh_count
+
+    # rename first and last column in the dictionary
+    columnnames.update(
+        {(list(columnnames))[0]: 'dataset', (list(columnnames))[1]: 'graphId', (list(columnnames))[2]: 'graphLabel',
+         (list(columnnames))[3]: 'filtrTime'})
+    feature_data.rename(columns=columnnames, inplace=True)  # give column names to dataframe
+
+    # write dataframe to file
+    feature_data.to_csv(
+        "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/result/filtration/" + dataset + str(
+            step_size) + "_ripsbetti.csv", index=False)
+
+
+def main(distance, step_size, dataset):
+    unique_graph_indicator, graph_labels, df_edges, graph_indicators = reading_csv(dataset)
+    alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, dataset, graph_labels, distance)
+
+
+if __name__ == '__main__':
+    data_path = "/home/taiwo/projects/def-cakcora/taiwo/data"  # dataset path on computer
+    data_list = ('ENZYMES', 'BZR', 'MUTAG', 'PROTEINS', 'DHFR', 'NCI1', 'COX2', 'REDDIT-MULTI-5K', 'REDDIT-MULTI-12K')
+    for ele in data_list:
+        for st in (10, 20, 50, 100):  # we will consider step size 100 for epsilon
+            main(step_size=st, distance="resistance", dataset=ele)

34f8bc8181ac4233faea06cd0b2d01b5f55ea634
Phunmey <funmilola@aims.edu.gh>
2023-10-12 14:15:50 -0500

    update

--- src/robustFiltration/rips_bettig_filtration1.py
+++ src/robustFiltration/rips_bettig_filtration1.py
@@ -59,8 +59,7 @@ def alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, da
         train_dgm_1 = train_rips[1]
 
         # save the persistence diagrams
-        filename = "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/persistentDiagram/" + dataset + str(
-            step_size) + "ripsbettig1.txt"
+        filename = "save PD"
         with open(filename, "a") as f:
             f.write(f"{graph_id}: {train_dgm_1}\n")
             f.flush()  # ensures data is written to file
@@ -120,9 +119,7 @@ def alpha_filt(unique_graph_indicator, graph_indicators, df_edges, step_size, da
     feature_data.rename(columns=columnnames, inplace=True)  # give column names to dataframe
 
     # write dataframe to file
-    feature_data.to_csv(
-        "/home/taiwo/projects/def-cakcora/taiwo/Apr2023/result/filtration/" + dataset + str(
-            step_size) + "_ripsbettig1.csv", index=False)
+    feature_data.to_csv("save dataframe", index=False)
 
 
 def main():
@@ -131,7 +128,7 @@ def main():
 
 
 if __name__ == '__main__':
-    data_path = "/home/taiwo/projects/def-cakcora/taiwo/data"  # dataset path on computer
+    data_path = "path to data"  # dataset path on computer
     data_list = ('ENZYMES', 'BZR', 'MUTAG', 'PROTEINS', 'DHFR', 'NCI1', 'COX2', 'REDDIT-MULTI-5K', 'REDDIT-MULTI-12K')
     for dataset in data_list:
         for step_size in (10, 20, 50, 100):  # we will consider step size 100 for epsilon
